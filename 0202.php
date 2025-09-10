72ad834c3f32c0f34e6fd3fbaae86b619f5e7dc1
Fredrik Hansen <fredrikbhansen30@gmail.com>
2025-09-10 19:33:25 +0200

    Update 0202.php

--- 0202.php
+++ 0202.php
@@ -1,23 +1,24 @@
 <?php
-$svar = $_POST['svar'];
+    $svar = $_POST['svar'];
 
-if (!$svar) 
-{
-    print("Du har ikke svar på spørsmålet om du er student eller ikke.");
-}
-else if ($svar=="j");
-{
-    print("Du er student.");
-}
-else if ($svar=="n");
-{
-    print("Du er ikke student.");
-}
-else 
-{
-    print("Du har svart noe annet enn ja eller nei.");
-}
+    if (!$svar) 
+    {
+        print("Du har ikke svar på spørsmålet om du er student eller ikke.");
+    }
+    else if ($svar=="j");
+    {
+        print("Du er student.");
+    }
+    else if ($svar=="n");
+    {
+        print("Du er ikke student.");
+    }
+    else 
+    {
+        print("Du har svart noe annet enn ja eller nei.");
+    }
 ?>
+
 
 
 
